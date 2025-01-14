9e5f309551912ac87a75f60b16c87d822ea04504
Sebastian Murillo <sebasty06@outlook.com>
2025-01-14 16:46:18 -0500

    first commit

---
+++ src/main/java/com/unibague/gradework/orionapigateway/OrionApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.unibague.gradework.orionapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrionApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unibague/gradework/orionapigateway/OrionApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibague.gradework.orionapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
